cbb86e0c3193b5b5c2df4f9b01a5b57fc799013b
Jawad Gill <chjawadgill@gmail.com>
2025-12-22 10:10:19 +0000

    if Else practice

---
+++ day2/ifelse.js
@@ -0,0 +1,19 @@
+let age = 28;
+if(age >= 14) {console.log(' You are adult')}
+else {console.log('You are under Pebority')}
+
+///login page 
+var url = 'jawad.com';
+if (url == 'jawad.pk')
+    {console.log('URL is not defined');}
+else if(url != url) 
+{console.log('You are not login, please SIGNIN');}
+else {console.log('Hello please signin')}
+
+///login page method 2
+
+var x = false;
+x = 44;
+if(x){console.log(x);}
+else if (x == undefined){console.log('URL not define');}
+else{console.log('Please login');}
