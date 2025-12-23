d1132f16f296b921ed5bcba3a10ce0f6fedd63ef
Jawad Gill <chjawadgill@gmail.com>
2025-12-23 08:57:41 +0000

    Day 3 ATM program

--- day2/ifelse.js
+++ day2/ifelse.js
@@ -1,19 +1,27 @@
-let age = 28;
-if(age >= 14) {console.log(' You are adult')}
-else {console.log('You are under Pebority')}
+// let age = 28;
+// if(age >= 14) {console.log(' You are adult')}
+// else {console.log('You are under Pebority')}
 
-///login page 
-var url = 'jawad.com';
-if (url == 'jawad.pk')
-    {console.log('URL is not defined');}
-else if(url != url) 
-{console.log('You are not login, please SIGNIN');}
-else {console.log('Hello please signin')}
+// ///login page 
+// var url = 'jawad.com';
+// if (url == 'jawad.pk')
+//     {console.log('URL is not defined');}
+// else if(url != url) 
+// {console.log('You are not login, please SIGNIN');}
+// else {console.log('Hello please signin')}
 
-///login page method 2
+// ///login page method 2
 
-var x = false;
-x = 44;
-if(x){console.log(x);}
-else if (x == undefined){console.log('URL not define');}
-else{console.log('Please login');}
+// var x = false;
+// x = 44;
+// if(x){console.log(x);}
+// else if (x == undefined){console.log('URL not define');}
+// else{console.log('Please login');}
+
+var userToken = true; // set after login
+var url = 'hhhh' ;
+if (url) {
+  console.log(url);
+} else {
+  console.log("Please try to login, you are not logged in");
+}
