8436b17401a4e1ca2487033dafc18cb1092fa3a0
Jawad Gill <chjawadgill@gmail.com>
2025-12-30 15:51:00 +0000

    4thday for rewise

---
+++ day4/rep.js
@@ -0,0 +1,17 @@
+add = (x,y)=>{
+    add = x+y;
+    return add
+}
+
+sub = (x,y)=>
+    {
+    if (y==0) {
+        return "No can not be devided by 0"}
+    if (y > x){
+        return "Devider must be less then number"}
+    sub = x/y
+    return sub
+    }
+
+
+
